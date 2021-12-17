a7c17d4250bbfb264988832e935610f221c0ee54
karla-walker <indiablonix@gmail.com>
2021-12-16 22:16:24 -0800

    Add watch target and passthrough copy for css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./src/sass/');
+  eleventyConfig.addPassthroughCopy('./src/css');
+
   return {
     dir: {
       input: 'src',

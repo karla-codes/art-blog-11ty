3669f150861bb8bd175efa4820d2e31557ac299d
karla-walker <indiablonix@gmail.com>
2021-12-29 13:53:23 -0800

    Install luxon and update date format for blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("sass/");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+  });
 
   return {
     dir: {
